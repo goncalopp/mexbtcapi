4fc53807562536740a0bc951f2749ac3eefe051c
goncalopp <goncalopp@pekkala.cp.quorumverita.com>
2012-09-29 03:14:35 +0100

    new constant_generator (with possibility of non-integer values)

--- mexbtcapi/util/constant_generator.py
+++ mexbtcapi/util/constant_generator.py
@@ -1,8 +1,11 @@
-def constant_generator(l, locals_dictionary):
-    '''given a string list ( a list of names of constants), and the locals(),
-    this function assigns each constant a number and registers them as variables'''
-    values= range( len(l) )         #the constants' values - integers
-    forward= dict( zip(l, values))  #name to number lookup
-    reverse= l                      #number to name lookup
+def constant_generator(locals_dictionary, keys, values=None):
+    '''given the locals(), a string list ( keys - a list of names of 
+    constants), and their values, this function assigns each constant 
+    it's value (or an integer, if values=None) and registers them as 
+    variables'''
+    if values==None:
+        values= range( len(keys) )     #the constants' values - integers
+    forward= dict( zip(keys, values))  #name to number lookup
+    reverse= keys                      #number to name lookup
     locals_dictionary['list']= reverse
     locals_dictionary.update( forward ) #register variables
