a3d448bb37ed9ed2086207778215909eed7638d0
goncalopp <goncalopp@pekkala.cp.quorumverita.com>
2012-09-29 17:11:30 +0100

    market: ticker

--- mexbtcapi/concepts/market.py
+++ mexbtcapi/concepts/market.py
@@ -1,3 +1,6 @@
+import datetime
+from currency import ExchangeRate
+
 class Trade(object):
     '''Represents an exchange of two currency amounts. May include the 
     entities between which the trade is made'''
@@ -16,6 +19,10 @@ class Market(object):
         '''c1 is the "buy" currency'''
         self.name= market_name
         self.c1, self.c2= c1, c2
+    
+    def getTicker(self):
+        '''returns the most recent ticker'''
+        raise NotImplementedError()
     
     def getOpenTrades(self):
         '''returns a list with all the open Trade's in the market'''
@@ -45,6 +52,25 @@ class ActiveParty( Party ):
     class TradeAlreadyClosed( Exception ):
         '''occurs when trying to cancel a already-closed trade'''
         pass
+
+class Ticker( object ):
+    '''A ticker datapoint.'''
+    TIME_PERIOD= 24*60*60   #time period (in seconds) associated with the
+                            #returned results: high, low, average, 
+                            #last, sell, buy, volume
+    def __init__(self, market, time, high=None, low=None, average=None, last=None, sell=None, buy=None, volume=None):
+        '''market: the market this ticker is associated with
+        time: the time at which this ticker was retrieved. This is preferably the server time, if available.
+        high, low,average,last,sell,buy: ExchangeRate.
+        '''
+        assert isinstance(market, Market)
+        assert all( [isinstance(x, ExchangeRate) for x in (high,low, average, last, sell, buy)] )
+        assert (volume is None) or (type(volume)==long)
+        assert all( [isinstance(x, ExchangeRate) for x in (high,low, average, last, sell, buy)] )
+        assert (buy is None and sell is None) or (buy<=sell) #market invariant. The market always buys a currency cheaper than it sells it
+        assert isinstance(time, datetime.datetime)
+        self.market, self.time, self.volume= market, time, volume
+        self.high, self.low,self.average,self.last, self.sell, self.buy= high, low, average, last, sell, buy
 
 class NotAuthorized( Exception ):
     '''Occurs when the user is not authorized to do the requested 
