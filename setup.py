7523bc07c3a0e796a2f9181481ece4c2fbdc31b6
goncalopp <goncalopp@pekkala.cp.quorumverita.com>
2012-09-29 02:11:25 +0100

    new setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 import setuptools
-setup(name='mtgox',
-      version='1.0.2',
-      description="Interface to the MtGox Bitcoin trading platform",
-      author="Emery Hemingway",
-      author_email="emery@vfemail.net",
-      url='https://github.com/3M3RY/python-mtgox',
-      packages = ['mtgox']
+setup(name='mexbtcapi',
+      version='0.1',
+      description="The Multi-Exchange Bitcoin API",
+      author="Goncalo Pinheira",
+      author_email="goncalopp@gmail.com",
+      url='hhttps://github.com/goncalopp/mexbtcapi',
+      packages = ['mexbtcapi']
       )
