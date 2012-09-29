4b4398a9d4b777fbb9f557e00fdeca1c04b9fb4f
goncalopp <goncalopp@pekkala.cp.quorumverita.com>
2012-09-29 17:11:54 +0100

    mtgox: initial high_level api, based on http_v1

---
+++ mexbtcapi/api/mtgox/http_v1/high_level.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+import functools
+import datetime
+
+import mexbtcapi
+from mexbtcapi import concepts
+BTC= concepts.currencies.BTC
+
+import mtgox as low_level
+
+MARKET_NAME= "MtGox"
+
+class MtgoxTicker( concepts.market.Ticker):
+    TIME_PERIOD= 24*60*60
+
+class Market( concepts.market.Market ):
+    def __init__( self, currency ):
+        mexbtcapi.concepts.market.Market.__init__(self, MARKET_NAME, BTC, currency)
+        self.multiplier= low_level.multiplier[ currency.name ] #to convert low level data
+        self.xchg_factory= functools.partial( concepts.currency.ExchangeRate, BTC, currency)
+
+    def getTicker(self):
+        data= low_level.ticker( self.c2.name )
+        data2= [ (Decimal(data[name]['value_int']) / self.multiplier) for name in ('high', 'low', 'avg', 'last', 'sell', 'buy') ]
+        hi,lo,av,la,se,bu= map( self.xchg_factory, data2 )
+        volume= long( data['vol']['value_int'] )
+        time= datetime.datetime.now()
+        return MtgoxTicker( market=self, time=time, high=hi, low=lo, average=av, last=la, sell=se, buy=bu ) 
