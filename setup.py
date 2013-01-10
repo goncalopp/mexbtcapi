d55a7b74425184a7aa6408a305c9225d49e67240
goncalopp <goncalopp@pekkala.cp.quorumverita.com>
2013-01-10 22:18:42 +0000

    setup.py metadata, LICENSE

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README')
+
 setup(name='mexbtcapi',
       version='0.1',
       description="The Multi-Exchange Bitcoin API",
+      long_description = README,
       author="Goncalo Pinheira",
-      author_email="goncalopp@gmail.com",
+      author_email="goncalopp+pypi@quorumverita.com",
       url='https://github.com/goncalopp/mexbtcapi',
       packages = ['mexbtcapi',
                   'mexbtcapi.util',
@@ -12,5 +19,14 @@ setup(name='mexbtcapi',
                   'mexbtcapi.api.mtgox',
                   'mexbtcapi.api.mtgox.http_v1',
                   'mexbtcapi.api.mtgox.streaming',
-                 ]
+                 ],
+      classifiers = \
+              [
+              'Development Status :: 2 - Pre-Alpha',
+              'Intended Audience :: Developers',
+              'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Software Development :: Libraries',
+               ]
       )
