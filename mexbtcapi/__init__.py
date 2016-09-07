a21ba7ae76430429e27b78b6aa90c893a106a6e1
goncalopp <goncalopp@hp-elitebook-840>
2016-09-07 10:58:47 +0100

    disable closed exchanges

--- mexbtcapi/__init__.py
+++ mexbtcapi/__init__.py
@@ -1,12 +1,13 @@
-from api import mtgox
-from api import bitcoin24
+#from api import mtgox
+#from api import bitcoin24
 from api import bitstamp
 import logging
 
 logging.basicConfig()
 logging.getLogger(__name__)
 
-apis = [mtgox,
-		bitcoin24,
-		bitstamp,
-		]
+apis = [
+    #mtgox,       #closed in 2013
+    #bitcoin24,   #closed in april 2013
+    bitstamp,
+    ]
