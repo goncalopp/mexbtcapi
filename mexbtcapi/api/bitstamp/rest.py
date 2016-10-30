158f5f8fc8bd40ac146048b8f1b61d2e0809c7a5
goncalopp <goncalopp@users.noreply.github.com>
2016-10-30 04:21:32 +0000

    rewrite bitstamp api

---
+++ mexbtcapi/api/bitstamp/rest.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+import datetime
+
+import mexbtcapi
+from mexbtcapi.concepts.currencies import BTC, USD, EUR
+from mexbtcapi.concepts.currency import Amount, ExchangeRate
+from mexbtcapi.concepts.market import Market as Market, PassiveParticipant, Ticker
+
+import urllib
+import urllib2
+import json
+
+CURRENCIES = {USD:'usd', EUR:'eur'}
+BASE_URL = 'https://www.bitstamp.net/api/v2/{method}/btc{currency}/'
+
+def request(url):
+    r = urllib2.urlopen(url)
+    data = json.load(r)
+    return data
+
+class BitstampTicker(Ticker):
+    TIME_PERIOD = datetime.timedelta(days=1)
+    RATE_FIELDS = Ticker.RATE_FIELDS + ('last', 'high', 'low', 'vwap', 'open')
+    NUMBER_FIELDS = Ticker.NUMBER_FIELDS + ('volume',)
+
+class BitstampMarket(Market):
+    def __init__(self, currency):
+        mexbtcapi.concepts.market.Market.__init__(self, 'Bitstamp', BTC, currency)
+        self.curr_code = CURRENCIES[currency]
+
+    def create_er(self, rate):
+        return ExchangeRate(numerator_currency=self.counter_currency, denominator_currency=BTC, rate=rate)
+
+    def get_ticker(self):
+        url = BASE_URL.format(currency=self.curr_code, method='ticker')
+        data = request(url)
+        time = datetime.datetime.utcfromtimestamp(float(data['timestamp']))
+
+        rates = {k: self.create_er(data[k]) for k in BitstampTicker.RATE_FIELDS}
+        numbers = {k: Decimal((data[k])) for k in BitstampTicker.NUMBER_FIELDS}
+        d = {} ; d.update(rates) ; d.update(numbers)
+        return BitstampTicker(market=self, time=time, **d) 
+
+    def get_orderbook(self):
+        raise NotImplementedError
+
+    def authenticate(self):
+        raise NotImplementedError
