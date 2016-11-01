debd744bb1d5efb588e3a00377785349c45e267e
goncalopp <goncalopp@users.noreply.github.com>
2016-11-01 03:31:47 +0000

    pubsub - MultichannelPublisher and nested Publishers

--- mexbtcapi/pubsub.py
+++ mexbtcapi/pubsub.py
@@ -6,21 +6,23 @@ import six
 log = logging.getLogger(__name__)
 
 
-@six.add_metaclass(ABCMeta)
 class Publisher(object):
     '''This class emits events'''
     def __init__(self):
         self._inactive_subscriptions = set()
         self._active_subscriptions = set()
+        self._start_callbacks = []
+        self._stop_callbacks = []
+        self._active = False
 
-    def subscribe(self, callback, start=True):
-        sub = Subscription(self, callback)
+    def subscribe(self, subscriber, start=True):
+        sub = Subscription(self, subscriber)
         self._inactive_subscriptions.add(sub)
         if start:
             sub.start()
         return sub
 
-    def is_active(self, subscription):
+    def is_subscription_active(self, subscription):
         in_active = subscription in self._active_subscriptions
         in_inactive = subscription in self._inactive_subscriptions
         if (not in_active) and (not in_inactive):
@@ -29,7 +31,7 @@ class Publisher(object):
 
     def start_subscription(self, subscription):
         '''Starts a Subscription, making it active'''
-        if self.is_active(subscription):
+        if self.is_subscription_active(subscription):
             raise Subscription.StateException("Subscription already started: {}".format(subscription))
         starting = len(self._active_subscriptions) == 0
         self._inactive_subscriptions.remove(subscription)
@@ -39,7 +41,7 @@ class Publisher(object):
 
     def stop_subscription(self, subscription):
         '''Stops a Subscription, making it inactive'''
-        if not self.is_active(subscription):
+        if not self.is_subscription_active(subscription):
             raise Subscription.StateException("Subscription already stopped: {}".format(subscription))
         stopping = len(self._active_subscriptions) == 1
         self._active_subscriptions.remove(subscription)
@@ -47,36 +49,51 @@ class Publisher(object):
         if stopping:
             self._stop()
 
+    def add_start_callback(self, f):
+        self._start_callbacks.append(f)
+
+    def add_stop_callback(self, f):
+        self._stop_callbacks.append(f)
+
     def send_message(self, message):
         '''Makes this publisher send a message to its subscribers'''
-        subscriptions = set(self._active_subscriptions) # make a copy, as original might be modified while running
+        subscriptions = tuple(self._active_subscriptions) # make a copy, as original might be modified while running
         for subscription in subscriptions:
-            subscription.callback(message)
+            subscription.send_message(message)
+    
+    @property
+    def active(self):
+        return self._active
 
-    @abstractmethod
     def _start(self):
-        pass
+        self._active = True
+        for f in self._start_callbacks:
+            f()
 
-    @abstractmethod
     def _stop(self):
-        pass
+        self._active = False
+        for f in self._stop_callbacks:
+            f()
 
 
 class Subscription(object):
-    '''This class represents an association between a Publisher and a callback function'''
+    '''This class represents an association between a Publisher and a subscriber.
+    The subscriber can be either a callback function or another publisher'''
     class StateException(Exception):
         pass
 
-    def __init__(self, publisher, callback):
+    def __init__(self, publisher, subscriber):
         assert isinstance(publisher, Publisher)
-        assert callable(callback)
+        assert callable(subscriber) or isinstance(subscriber, Publisher)
         self.publisher = publisher
-        self.callback = callback
-        self._active = False
+        self.subscriber = subscriber
+        if isinstance(subscriber, Publisher):
+            subscriber.add_start_callback(self.start)
+            subscriber.add_stop_callback(self.stop)
 
     @property
     def active(self):
-        return self.publisher.is_active(self)
+        return self.publisher.is_subscription_active(self)
 
     def start(self):
         self.publisher.start_subscription(self)
@@ -84,3 +101,31 @@ class Subscription(object):
     def stop(self):
         self.publisher.stop_subscription(self)
 
+    def send_message(self, message):
+        if isinstance(self.subscriber, Publisher):
+            self.subscriber.send_message(message)
+        else:
+            self.subscriber(message)
+
+class MultichannelPublisher(Publisher):
+    def __init__(self):
+        Publisher.__init__(self)
+        self._channel_to_sub = {}
+
+    def _get_or_create_channel_subscription(self, channel_name):
+        if not channel_name in self._channel_to_sub:
+            channel_pub = Publisher()
+            channel_sub = Subscription(self, channel_pub)
+            self._channel_to_sub[channel_name] = channel_sub
+            self._inactive_subscriptions.add(channel_sub)
+        return self._channel_to_sub[channel_name]
+
+    def send_message(self, message, channel):
+        sub = self._get_or_create_channel_subscription(channel)
+        sub.send_message(message)
+
+    def subscribe(self, subscriber, channel, start=True):
+        sub = self._get_or_create_channel_subscription(channel)
+        channel_pub = sub.subscriber
+        assert isinstance(channel_pub, Publisher)
+        return channel_pub.subscribe(subscriber, start=start)
--- mexbtcapi/test/test_pubsub.py
+++ mexbtcapi/test/test_pubsub.py
@@ -2,20 +2,13 @@
 
 import unittest
 from mock import MagicMock
-from mexbtcapi.pubsub import Publisher, Subscription
-
-class SimplePublisher(Publisher):
-    '''Implements abstract methods'''
-    def _start(self):
-        pass
-    def _stop(self):
-        pass
+from mexbtcapi.pubsub import Publisher, Subscription, MultichannelPublisher
 
 
 class PublisherTest(unittest.TestCase):
     @classmethod
     def _get_subscribed_and_send(cls, publisher=None, start=True, message='novalue'):
-        p = publisher or SimplePublisher()
+        p = publisher or Publisher()
         subscriber = MagicMock()
         m = message if message != 'novalue' else cls._get_test_messages()[0]
         subscription = p.subscribe(subscriber, start=start)
@@ -27,7 +20,7 @@ class PublisherTest(unittest.TestCase):
         return "message1", 2, False
 
     def test_create(self): 
-        p = SimplePublisher()
+        p = Publisher()
 
     def test_subscribe_simple(self):
         p, subscriber, subscription = self._get_subscribed_and_send()
@@ -119,10 +112,200 @@ class PublisherTest(unittest.TestCase):
         p.send_message(m1)
         self.assertEqual(subscriber.call_count, 2)
 
+    def test_start_stop_callbacks(self):
+        p, subscriber1, subscription1 = self._get_subscribed_and_send(start=False)
+        start_cb, stop_cb = MagicMock(), MagicMock()
+        p.add_start_callback(start_cb)
+        p.add_stop_callback(stop_cb)
+        _, subscriber2, subscription2 = self._get_subscribed_and_send(publisher=p, start=False)
+        self.assertEqual(start_cb.call_count, 0)
+        self.assertEqual(stop_cb.call_count, 0)
+        subscription1.start()
+        self.assertEqual(start_cb.call_count, 1)
+        self.assertEqual(stop_cb.call_count, 0)
+        subscription2.start()
+        self.assertEqual(start_cb.call_count, 1)
+        self.assertEqual(stop_cb.call_count, 0)
+        subscription1.stop()
+        self.assertEqual(start_cb.call_count, 1)
+        self.assertEqual(stop_cb.call_count, 0)
+        subscription2.stop()
+        self.assertEqual(start_cb.call_count, 1)
+        self.assertEqual(stop_cb.call_count, 1)
+        subscription2.start()
+        self.assertEqual(start_cb.call_count, 2)
+        self.assertEqual(stop_cb.call_count, 1)
+        subscription2.stop()
+        self.assertEqual(start_cb.call_count, 2)
+        self.assertEqual(stop_cb.call_count, 2)
 
+    def test_active(self):
+        p, subscriber1, subscription1 = self._get_subscribed_and_send(start=False)
+        _, subscriber2, subscription2 = self._get_subscribed_and_send(publisher=p, start=False)
+        self.assertEqual(p.active, False)
+        subscription1.start()
+        self.assertEqual(p.active, True)
+        subscription2.start()
+        self.assertEqual(p.active, True)
+        subscription1.stop()
+        self.assertEqual(p.active, True)
+        subscription2.stop()
+        self.assertEqual(p.active, False)
+        subscription2.start()
+        self.assertEqual(p.active, True)
+        subscription2.stop()
+        self.assertEqual(p.active, False)
 
+class PublisherNestedTest(unittest.TestCase):
+    def test_nested_simple(self):
+        np1_sub1 = MagicMock()
+        np1_sub2 = MagicMock()
+        np2_sub1 = MagicMock()
+        p = Publisher()
+        np1 = Publisher()
+        np2 = Publisher()
+        np1_subs = p.subscribe(np1, start=False)
+        np2_subs = p.subscribe(np2, start=False)
+        self.assertEqual(p.active, False)
+        self.assertEqual(np1.active, False)
+        self.assertEqual(np2.active, False)
+        #subscribe to np1
+        np1.subscribe(np1_sub1, start=False)
+        np1.subscribe(np1_sub2, start=False)
+        self.assertEqual(p.active, False)
+        self.assertEqual(np1.active, False)
+        self.assertEqual(np2.active, False)
+        np1_sub1_subs = np1.subscribe(np1_sub1)
+        self.assertEqual(p.active, True)
+        self.assertEqual(np1.active, True)
+        self.assertEqual(np2.active, False)
+        np1_sub2_subs = np1.subscribe(np1_sub2)
+        self.assertEqual(p.active, True)
+        self.assertEqual(np1.active, True)
+        self.assertEqual(np2.active, False)
+        #send message to p
+        p.send_message(1)
+        np1_sub1.assert_called_with(1)
+        np1_sub2.assert_called_with(1)
+        self.assertEqual(np2_sub1.call_count, 0)
+        #subscribe to np2
+        np2_sub1_subs = np2.subscribe(np2_sub1)
+        self.assertEqual(p.active, True)
+        self.assertEqual(np1.active, True)
+        self.assertEqual(np2.active, True)
+        #send message to p
+        p.send_message(2)
+        np1_sub1.assert_called_with(2)
+        np1_sub2.assert_called_with(2)
+        np2_sub1.assert_called_with(2)
+        self.assertEqual(np1_sub1.call_count, 2)
+        self.assertEqual(np1_sub2.call_count, 2)
+        self.assertEqual(np2_sub1.call_count, 1)
+        #unsubscribe to np2
+        np2_sub1_subs.stop()
+        self.assertEqual(p.active, True)
+        self.assertEqual(np1.active, True)
+        self.assertEqual(np2.active, False)
+        #send message to p
+        p.send_message(3)
+        np1_sub1.assert_called_with(3)
+        np1_sub2.assert_called_with(3)
+        np2_sub1.assert_called_with(2)
+        self.assertEqual(np1_sub1.call_count, 3)
+        self.assertEqual(np1_sub2.call_count, 3)
+        self.assertEqual(np2_sub1.call_count, 1)
+        #unsubscribe to np1_sub2
+        np1_sub2_subs.stop()
+        self.assertEqual(p.active, True)
+        self.assertEqual(np1.active, True)
+        self.assertEqual(np2.active, False)
+        #send message to p
+        p.send_message(4)
+        np1_sub1.assert_called_with(4)
+        np1_sub2.assert_called_with(3)
+        np2_sub1.assert_called_with(2)
+        self.assertEqual(np1_sub1.call_count, 4)
+        self.assertEqual(np1_sub2.call_count, 3)
+        self.assertEqual(np2_sub1.call_count, 1)
+        #unsubscribe to np1_sub1
+        np1_sub1_subs.stop()
+        self.assertEqual(p.active, False)
+        self.assertEqual(np1.active, False)
+        self.assertEqual(np2.active, False)
+        #send message to p
+        p.send_message(5)
+        np1_sub1.assert_called_with(4)
+        np1_sub2.assert_called_with(3)
+        np2_sub1.assert_called_with(2)
+        self.assertEqual(np1_sub1.call_count, 4)
+        self.assertEqual(np1_sub2.call_count, 3)
+        self.assertEqual(np2_sub1.call_count, 1)
 
-
+class MultichannelPublisherTest(unittest.TestCase):
+    def test_simple(self):
+        c1_sub1 = MagicMock()
+        c1_sub2 = MagicMock()
+        c2_sub1 = MagicMock()
+        p = MultichannelPublisher()
+        self.assertEqual(p.active, False)
+        #send messages to random channels
+        p.send_message(9, "c1")
+        p.send_message(9, "c2")
+        p.send_message(9, "something")
+        p.send_message(9, "mychannel")
+        #subscribe to c1
+        c1_sub1_subs = p.subscribe(c1_sub1, "c1")
+        self.assertEqual(p.active, True)
+        c1_sub2_subs = p.subscribe(c1_sub2, "c1")
+        #send message to c1
+        p.send_message(1, "c1")
+        c1_sub1.assert_called_with(1)
+        c1_sub2.assert_called_with(1)
+        self.assertEqual(c2_sub1.call_count, 0)
+        #subscribe to c2
+        c2_sub1_subs = p.subscribe(c2_sub1, "c2")
+        #send message to c2
+        p.send_message(2, "c2")
+        c1_sub1.assert_called_with(1)
+        c1_sub2.assert_called_with(1)
+        c2_sub1.assert_called_with(2)
+        self.assertEqual(c1_sub1.call_count, 1)
+        self.assertEqual(c1_sub2.call_count, 1)
+        self.assertEqual(c2_sub1.call_count, 1)
+        #unsubscribe to c2
+        c2_sub1_subs.stop()
+        self.assertEqual(p.active, True)
+        #send message to c2
+        p.send_message(3, "c2")
+        c1_sub1.assert_called_with(1)
+        c1_sub2.assert_called_with(1)
+        c2_sub1.assert_called_with(2)
+        self.assertEqual(c1_sub1.call_count, 1)
+        self.assertEqual(c1_sub2.call_count, 1)
+        self.assertEqual(c2_sub1.call_count, 1)
+        #unsubscribe to c1_sub2
+        c1_sub2_subs.stop()
+        self.assertEqual(p.active, True)
+        #send message to c1
+        p.send_message(4, "c1")
+        c1_sub1.assert_called_with(4)
+        c1_sub2.assert_called_with(1)
+        c2_sub1.assert_called_with(2)
+        self.assertEqual(c1_sub1.call_count, 2)
+        self.assertEqual(c1_sub2.call_count, 1)
+        self.assertEqual(c2_sub1.call_count, 1)
+        #unsubscribe to c1_sub1
+        c1_sub1_subs.stop()
+        self.assertEqual(p.active, False)
+        #send message to c1
+        p.send_message(5, "c1")
+        c1_sub1.assert_called_with(4)
+        c1_sub2.assert_called_with(1)
+        c2_sub1.assert_called_with(2)
+        self.assertEqual(c1_sub1.call_count, 2)
+        self.assertEqual(c1_sub2.call_count, 1)
+        self.assertEqual(c2_sub1.call_count, 1)
+        self.assertEqual(p.active, False)
 
 if __name__ == '__main__':
     unittest.main()
