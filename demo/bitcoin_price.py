5007b83277bf06fa3adf4f2e645ba6e98aea34bc
goncalopp <goncalopp@users.noreply.github.com>
2016-11-02 01:25:20 +0000

    fix demo/bitcoin_price.py

--- demo/bitcoin_price.py
+++ demo/bitcoin_price.py
@@ -8,7 +8,7 @@ for market in mexbtcapi.markets.find(BTC):
     try:
         exchange_rate= market.get_ticker().ask
         got = exchange_rate.convert( bitcoins )
-        print "At %s I can get %s for my %s (that's %s)"%(market.name, got, bitcoins, exchange_rate)
+        print "At %s I can get %s for my %s (that's %s)"%(market.exchange, got, bitcoins, exchange_rate)
     except Exception as e:
         print "Failed to use "+market.full_name 
         print e
