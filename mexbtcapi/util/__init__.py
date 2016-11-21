098da4d67e4be8ddea9aeac8d718f653bdc54a23
goncalopp <goncalopp@users.noreply.github.com>
2016-11-21 23:33:33 +0000

    remove old util code to deprecated/

--- mexbtcapi/util/__init__.py
+++ mexbtcapi/util/__init__.py
@@ -4,6 +4,18 @@ from collections import defaultdict
 
 logging.getLogger(__name__)
 
+def constant_generator(locals_dictionary, keys, values=None):
+    """given the locals(), a string list ( keys - a list of names of
+    constants), and their values, this function assigns each constant
+    it's value (or an integer, if values=None) and registers them as
+    variables"""
+    if values is None:
+        values = range(len(keys))       # the constants' values - integers
+    forward = dict(zip(keys, values))   # name to number lookup
+    reverse = keys                      # number to name lookup
+    locals_dictionary['list'] = reverse
+    locals_dictionary.update(forward)   # register variables
+
 def group_by(iterable, key_func=lambda x: x, multi=False):
     '''Groups elements by the output of a given key function.
     The key function output can be either a iterable (multi=True) or a non-iterable (multi=False).
